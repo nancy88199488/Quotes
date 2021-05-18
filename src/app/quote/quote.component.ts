ad866ab704ad4a50bbbd154c7336b59ef233cc4b
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-18 11:18:43 +0300

    updates and modifications of changes made

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, "Nancy","Success is most often achieved by those who do not know that failure is inevitable.", "By Coco Chanel", new Date(2021, 5,16),0, 0),
-    new Quote(3, "Nancy", "If you do what you’ve always done, you’ll get what you’ve always gotten.", " By Tony Robbins",new Date(2021, 5,16),0,0),
-    new Quote(4, "Nancy", "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",  "By Aristotle",new Date(2021, 5,16,),0,0),
-    new Quote(5, "Nancy", "Only those who will risk going too far can possibly find out how far one can go.", "By T. S. Eliot",new Date(2021, 5,16),0,0),
-    new Quote(6, "Nancy", "If you don’t build your dream, someone else will hire you to help them build theirs.", "By  Dhirubhai Ambani",new Date(2021, 5,16), 0,0),
-    new Quote(7,"Nancy", "Life is being on the wire, everything else is just waiting.", "By Karl Wallenda",new Date(2021, 5,16),0,0),
+    new Quote(1, "Nancy","Success is most often achieved by those who do not know that failure is inevitable.", "Coco Chanel", new Date(2021, 5,16),0, 0),
+    new Quote(3, "Nancy", "If you do what you’ve always done, you’ll get what you’ve always gotten.", "Tony Robbins",new Date(2021, 5,16),0,0),
+    new Quote(4, "Nancy", "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",  "Aristotle",new Date(2021, 5,16,),0,0),
+    new Quote(5, "Nancy", "Only those who will risk going too far can possibly find out how far one can go.", "T. S. Eliot",new Date(2021, 5,16),0,0),
+    new Quote(6, "Nancy", "If you don’t build your dream, someone else will hire you to help them build theirs.", "Dhirubhai Ambani",new Date(2021, 5,16), 0,0),
+    new Quote(7,"Nancy", "Life is being on the wire, everything else is just waiting.", "Karl Wallenda",new Date(2021, 5,16),0,0),
   ];
 
     toggleQuotes(index){
