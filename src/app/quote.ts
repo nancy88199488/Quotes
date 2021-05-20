fda3cd58bbfea6ba3a9fae8ab8d0798f9355a130
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-20 22:46:15 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string, public author:string, public completeDate: Date,  public upvote:number, public downvote:number ){
-      this.showDescription=false;
-    }
-  }
